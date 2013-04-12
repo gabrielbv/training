24be84f18efb130c701ef875d4d3803905caf2bc
Gabriel <gabriel@gabriel-desktop.(none)>
2013-04-12 18:45:05 +0300

    Admin edits

--- training/settings.py
+++ training/settings.py
@@ -1,4 +1,8 @@
 # Django settings for training project.
+import os
+import sys
+
+PROJECT_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -119,6 +123,9 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 TEMPLATE_DIRS = (
+
+   os.path.join(PROJECT_ROOT, 'templates'),
+
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
